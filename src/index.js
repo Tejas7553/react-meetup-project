e2f088f472fff658d209cb0528bd77973eded3c7
Tejas7553 <jadhav.tejas7553@gmail.com>
2023-11-03 21:27:12 +0530

    Five

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { FavoritesContextProvider } from "./store/favorites-context";
 
 // import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <FavoritesContextProvider>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </FavoritesContextProvider>
 );
